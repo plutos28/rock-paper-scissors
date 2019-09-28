a9146c6f32f4700a06acf7d49e90a0fe9e5471a9
plutos28 <48158371+plutos28@users.noreply.github.com>
2019-09-28 23:12:34 +0300

    change spacing in if statements

--- game.js
+++ game.js
@@ -8,25 +8,26 @@ function getRandomNumber(max) {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // to make sure all cases work(rock, RoCk, ROCK)
     playerSelection = playerSelection.toLowerCase()
 
-    if (playerSelection == "rock" && computerSelection == "scissors") {
+    if(playerSelection == "rock" && computerSelection == "scissors") {
         return "You Win! Rock beats Scissors"
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
+    }else if(playerSelection == "rock" && computerSelection == "paper") {
         return "You Lose! Paper beats Rock"
-    } else if (playerSelection == "rock" && computerSelection == "rock") {
+    }else if(playerSelection == "rock" && computerSelection == "rock") {
         return "It's a tie!"
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    }else if(playerSelection == "scissors" && computerSelection == "paper") {
         return "You Win! Scissors beats Paper"
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    }else if(playerSelection == "scissors" && computerSelection == "rock") {
         return "You Lose! Rock beats Scissors"
-    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    }else if(playerSelection == "scissors" && computerSelection == "scissors") {
         return "It's a tie!"
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
+    }else if(playerSelection == "paper" && computerSelection == "rock") {
         return "You Win! Paper beats Rock"
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    }else if(playerSelection == "paper" && computerSelection == "scissors") {
         return "You Lose! Scissors beats Paper"
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
+    }else if(playerSelection == "paper" && computerSelection == "paper") {
         return "It's a tie!"
     }
 }
